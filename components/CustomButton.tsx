3558009e0cecd0db4af18586653660f41a6b0f27
AR-Tausif <tausif.ritu1@gmail.com>
2023-07-30 21:36:35 +0600

    I faced typescript syntax error for render next js project in web with custom tailwindcss

---
+++ components/CustomButton.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const CustomButton = () => {
+  return (
+    <button className="custom-btn bg-purple-700 text-white rounded-lg">
+      there ones
+    </button>
+  );
+};
+
+export default CustomButton;
